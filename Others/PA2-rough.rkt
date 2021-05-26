0df56a2886c9e3a22fc4872151e2a3125ec7db78
Rahul Saini <rs4231199@gmail.com>
2021-05-26 15:23:08 +0530

    PA2 resubmission: All testcases passes in this submission. changes: removed unnecessary 'if's from stream definitions

--- Others/PA2-rough.rkt
+++ Others/PA2-rough.rkt
@@ -1,8 +1,16 @@
 #lang sicp
+(define (memo-proc proc)
+  (let ((already-run? false) (result false))
+    (lambda ()
+      (if (not already-run?)
+          (begin (set! result (proc))
+                 (set! already-run? true)
+                 result)
+          result))))
 
 (define-syntax delay
   (syntax-rules ()
-    ((delay expr) (lambda () expr))))
+    ((delay exp) (memo-proc (lambda () exp)))))
 
 (define-syntax cons-stream
   (syntax-rules ()
@@ -19,55 +27,46 @@
 
 (define (stream-null? s) (eq? s the-empty-stream))
 
-
-(define (stream-ref s n)
-  (if (= n 0)
-      (stream-car s)
-      (stream-ref (stream-cdr s) (- n 1))))
-
 (define (stream-map proc s)
   (if (stream-null? s)
       the-empty-stream
       (cons-stream (proc (stream-car s))
                    (stream-map proc (stream-cdr s)))))
 
+(define (stream-filter pred s)
+  (cond ((stream-null? s) the-empty-stream)
+        ((pred (stream-car s))
+         (cons-stream (stream-car s)
+                      (stream-filter pred (stream-cdr s))))
+        (else (stream-filter pred (stream-cdr s)))))
 
+(define (enumerate-interval low high)
+  (if (> low high)
+      'nil
+      (cons low (enumerate-interval (+ low 1) high))))
 
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream low
+                   (stream-enumerate-interval (+ low 1) high))))
+
+(define (square x) (* x x))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
 
 ;=======================ends here====================
 
-(define (get-from s n)
-  (define (get-favorite s n c1 c2 c3 c4)
-    (if (<= n 1)
-        (let ()
-          (cond
-            ((equal? (stream-car s) 1) (set! c1 (inc c1)))
-            ((equal? (stream-car s) 2) (set! c2 (inc c2)))
-            ((equal? (stream-car s) 3) (set! c3 (inc c3)))
-            ((equal? (stream-car s) 4) (set! c4 (inc c4))))
-          (define favorite 4)
-          (define its-count 0)
-          (if(> c4 its-count) (begin (set! favorite 4) (set! its-count c4)))
-          (if(> c3 its-count) (begin (set! favorite 3) (set! its-count c3)))
-          (if(> c2 its-count) (begin (set! favorite 2) (set! its-count c2)))
-          (if(> c1 its-count) (begin (set! favorite 1) (set! its-count c1)))
-          favorite)
-        (let ()
-          (cond
-            ((equal? (stream-car s) 1) (set! c1 (inc c1)))
-            ((equal? (stream-car s) 2) (set! c2 (inc c2)))
-            ((equal? (stream-car s) 3) (set! c3 (inc c3)))
-            ((equal? (stream-car s) 4) (set! c4 (inc c4))))
-          (get-favorite (stream-cdr s) (- n 1) c1 c2 c3 c4))))
 
-  (define (beats-favorite favorite)
-    (cond
-      ((equal? favorite 1) 4)
-      ((equal? favorite 2) 3)
-      ((equal? favorite 3) 4)
-      ((equal? favorite 4) 2)))
-  
-  (beats-favorite (get-favorite s n 0 0 0 0)))
 
 ; 2 2 3 10
 ;(define (fu n)
@@ -79,22 +78,17 @@
 ;(4 4 2 2 2 2 3 3 3 3)
 
 (define (fu n)
-  (cond ((= n 1)
-         (cons-stream 3 (fu (+ 1 n))))
-        (else
-         (cons-stream (get-from (pooh 1) (- n 1)) (fu (+ 1 n))))))
+  (cons-stream (stream-ref Pooh (- n 2)) (fu (+ 1 n))))
 
 (define (pooh n)
-  (cond ((= n 1)
-         (cons-stream 4 (pooh (+ 1 n))))
-        (else 
-         (cons-stream (get-from (fu 1) (- n 1)) (pooh (+ 1 n))))))
+  (cons-stream (stream-ref Fu (- n 2)) (pooh (+ 1 n))))
 
-;(stream-ref (fu 1) 1)
+(define Fu (cons-stream 2 (fu 2)))
+(define Pooh (cons-stream 4 (pooh 2)))
 
 (define (print s n)
   (if (= n 0)
       '()
       (cons (stream-car s) (print (stream-cdr s) (- n 1)))))
-(print (fu 1) 10) ;(3 2 2 2 2 2 2 4 4 4)
-(print (pooh 1) 10) ;(4 4 4 3 3 3 3 3 3 3)
+(print Fu 30) ;(3 2 2 2 2 2 2 4 4 4)
+;(print Pooh 10) ;(4 4 4 3 3 3 3 3 3 3)
